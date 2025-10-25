67b859874e274f5e90b4aaa55d5e8180e91a617c
HelenaYang <helena.yang@sorbonne-nouvelle.fr>
2025-10-25 20:09:05 +0200

    ajout Exercices/comptes.sh

---
+++ Exercices/comptes.sh
@@ -0,0 +1,14 @@
+!/usr/bin/bash
+
+echo "argument donné : $1"
+
+CHEMIN=$1
+
+echo "Nombre de lieux en 2016:"
+cat "$CHEMIN/ann2016/"* | grep Location | wc -l
+echo "Nombre de lieux en 2017:"
+cat "$CHEMIN/ann2017/"* | grep Location | wc -l
+echo "Nombre de lieux en 2018:"
+cat "$CHEMIN/ann2018/"* | grep Location | wc -l
+
+
