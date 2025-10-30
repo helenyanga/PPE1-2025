77c01c52efd67a06229cbae4339c4e20268c2885
HelenaYang <helena.yang@sorbonne-nouvelle.fr>
2025-10-30 14:37:07 +0100

    sauvegarde_erreurs-correction

--- Exercices/comptes.sh
+++ Exercices/comptes.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/bash
 
+#Résultats pour chaque année :
+#   9444   54332  300959
+#9444
+#   3144   16146   92409
+#Nombre de lieux en 2016:
+#3144
+#   7183   40683  229756
+#Nombre de lieux en 2017:
+#2466
+#   3110   16187   99894
+#Nombre de lieux en 2018:
+#3110
+
+
+#Correction :
 echo "argument donné : $1"
 
 CHEMIN=$1
@@ -10,5 +25,3 @@ echo "Nombre de lieux en 2017:"
 cat "$CHEMIN/ann2017/"* | grep Location | wc -l
 echo "Nombre de lieux en 2018:"
 cat "$CHEMIN/ann2018/"* | grep Location | wc -l
-
-
