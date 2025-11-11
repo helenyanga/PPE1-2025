606348be3281dae7c214a0944678d79057cb10b9
HelenaYang <helena.yang@sorbonne-nouvelle.fr>
2025-11-11 12:37:57 +0100

    correction1_mp1

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -4,32 +4,57 @@
 
 #On vérifie qu'on a un argument c'est-à-dire, que le fichier est bien un argument :
 #$1 : indique l'argument qui est donné, ici c'est le nom du fichier.
-FICHIER_FR=$1
+FICHIER_URL=$1
+FICHIER_SORTIE=$2
 N=0
 if [ $# -eq 0 ]
 then
     echo "Ce programme n'a pas d'argument."
-    echo "Vous devez donner le nom d'un fichier en tant qu'argument dans la Konsole : "
+    echo "Vous devez fournir un argument, dans la Konsole, en lui donnant le chemin absolu où se trouve le fichier que vous voulez utiliser."
+    echo "Pour ce faire, utiliser la commande suivante : ./nomdufichier argument"
+    echo "Si besoin, utiliser la commande 'pwd' pour avoir le chemin en entier."
     exit
 fi
 
-#Condition qui vérifie si le fichier donné existe bien, s'il n'existe pas il affichera erreur.
+#Condition qui vérifie si le fichier donné existe bien, s'il n'existe pas, il affichera erreur.
 if [ ! -f $1 ]
 then
-    echo "Erreur : le fichier "$1" n'existe pas."
+    echo "Erreur : le fichier "$1" n'existe pas. Recommencer."
     exit
 fi
 
-echo "Le fichier existe, c'est "$1"."
+echo "Le fichier existe, c'est "$1""
+
+#Condition qui vérifie si l'url est valide ou non.
+OK=0
+NOK=0
+while read -r line;
+do
+    echo "La ligne : $line";
+    if [[ $line =~ ^https?:// ]]
+    then
+        echo "Ressemble à une URL valide."
+        OK=$( expr $OK + 1 )
+    else
+        echo "Ne ressemble pas à une URL valide."
+        NOK=$( expr $NOK + 1 )
+    fi
+done < $FICHIER_URL;
+echo "$OK URLs et $NOK lignes douteuses."
+
+#On crée des variables pour l'HTTP et l'encodage.
+HTTP=$(curl -i -L -s -o "tmp.txt" -w "%${http_code}\n%${content_type}" "${line}")
+#(à la base : curl -i -L -s -w "%$2\n%$3" -o /dev/null ${line})
+ENCODAGE=$(curl -i -L -s -o "tmp.txt" -w "%${http_code}\n%${content_type}" "${line}")
+
+echo "On doit avoir comme résultats :"
+echo -e "Numéro_de_la_ligne\tHTTP \tEncodage_Charset\tNombre_de_mots > envoyer_dans_le_fichier_sortie "$FICHIER_SORTIE""
 
 #On veut lire ligne par ligne le contenu du fichier.
 while read -r line ;
 do
-    for LINE in ${line}
-    do
-        N=$( expr $N + 1 )
-        echo -e "$N \t" "${line} \t" "curl -I ${line} | grep - HTTP | wc -l \t" "curl -I  {line} | grep - content-type | wc -l \t" "curl -s ${line} | wc - w"
-    done
+    echo -e "${N}\t${line}\t${HTTP}"
+    N=$( expr $N + 1 )
 #commande pour le code HTTP de réponse à la requête et l'encodage de la page, s'il est présent : curl -I adresseurl
 #pour le nombre de mots dans l'url : curl -s adresseurl | wc - w
-done < "$1";
+done < $FICHIER_URL;
